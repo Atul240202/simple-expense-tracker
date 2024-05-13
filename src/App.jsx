40ea79294805d27b46bec00670eb1e10f4b31347
atul240202 <19bcs1752@gmail.com>
2024-05-13 11:58:55 +0530

    Minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
 import NewExpense from './components/NewExpense';
@@ -10,15 +8,15 @@ let dummy_expenses = [
     id: '1',
     title: 'Car Insurance',
     amount: 326,
-    date: new Date(2021, 2, 28),
+    date: new Date(2023, 2, 28),
   },
   {
     id: '2',
     title: 'Car Maintenance',
     amount: 100,
-    date: new Date(2021, 1, 28),
+    date: new Date(2023, 1, 28),
   },
-  { id: '3', title: 'Car EMI', amount: 56789, date: new Date(2021, 8, 28) },
+  { id: '3', title: 'Car EMI', amount: 56789, date: new Date(2023, 8, 28) },
 ];
 
 const App = () => {
--- src/components/ExpenseFilter.jsx
+++ src/components/ExpenseFilter.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './style/ExpenseFilter.css';
 
 const ExpensesFilter = (props) => {
@@ -11,6 +10,7 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2024'>2024</option>
           <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
